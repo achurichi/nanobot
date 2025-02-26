851b2c79a41e8690a4d58b28aa8e266e352e0ef0
Alan Churichi <achurichi@gmail.com>
2025-02-26 15:34:23 -0300

    Add simulation

---
+++ ros_ws/src/bringup/launch/robot.launch.py
@@ -0,0 +1,85 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+
+from launch_ros.actions import Node
+
+import xacro
+
+CONTROLER_PACKAGE_NAME = "control"
+DESCRIPTION_PACKAGE_NAME = "description"
+
+
+def generate_launch_description():
+    # get URDF via xacro
+    desc_pkg_path = os.path.join(get_package_share_directory(DESCRIPTION_PACKAGE_NAME))
+    xacro_file = os.path.join(desc_pkg_path, "urdf", "robot.urdf.xacro")
+    robot_description_config = xacro.process_file(xacro_file)
+    robot_description = {"robot_description": robot_description_config.toxml()}
+
+    # get controler
+    controler_pkg_path = os.path.join(
+        get_package_share_directory(CONTROLER_PACKAGE_NAME)
+    )
+    robot_controllers = os.path.join(
+        controler_pkg_path, "config", "diffdrive_controllers.yaml"
+    )
+
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_controllers],
+        output="both",
+        remappings=[
+            ("~/robot_description", "/robot_description"),
+            ("/diff_controller/cmd_vel", "/cmd_vel"),
+        ],
+    )
+    robot_state_pub_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "joint_state_broadcaster",
+            "--controller-manager",
+            "/controller_manager",
+        ],
+    )
+
+    robot_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "diff_controller",
+            "--controller-manager",
+            "/controller_manager",
+        ],
+    )
+
+    # Delay start of joint_state_broadcaster after `robot_controller`
+    # TODO(anyone): This is a workaround for flaky tests. Remove when fixed.
+    delay_joint_state_broadcaster_after_robot_controller_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=robot_controller_spawner,
+            on_exit=[joint_state_broadcaster_spawner],
+        )
+    )
+
+    return LaunchDescription(
+        [
+            control_node,
+            robot_state_pub_node,
+            robot_controller_spawner,
+            delay_joint_state_broadcaster_after_robot_controller_spawner,
+        ]
+    )
---
+++ ros_ws/src/bringup/launch/simulation.launch.py
@@ -0,0 +1,77 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+PACKAGE_NAME = "bringup"
+SIM_PACKAGE_NAME = "simulation"
+
+
+def generate_launch_description():
+    rsp = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory(PACKAGE_NAME), "launch", "rsp.launch.py"
+                )
+            ]
+        ),
+        launch_arguments={"use_sim_time": "true"}.items(),
+    )
+
+    gazebo_params_file = os.path.join(
+        get_package_share_directory(SIM_PACKAGE_NAME), "config", "gazebo_params.yaml"
+    )
+
+    # Include the Gazebo launch file, provided by the gazebo_ros package
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory("gazebo_ros"),
+                    "launch",
+                    "gazebo.launch.py",
+                )
+            ]
+        ),
+        launch_arguments={
+            "extra_gazebo_args": "--ros-args --params-file " + gazebo_params_file
+        }.items(),
+    )
+
+    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        arguments=["-topic", "robot_description", "-entity", "my_bot"],
+        output="screen",
+    )
+
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_controller"],
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
+
+    # Launch them all!
+    return LaunchDescription(
+        [
+            rsp,
+            gazebo,
+            spawn_entity,
+            diff_drive_spawner,
+            joint_broad_spawner,
+        ]
+    )
