347e53ab2ebd7539c291635494e0fb6425c45320
Alan Churichi <achurichi@gmail.com>
2025-03-01 14:32:12 -0300

    Add FastDDS discovery server

--- ros_ws/scripts/start.sh
+++ ros_ws/scripts/start.sh
@@ -20,6 +20,18 @@ if [ "$1" == "sim" ]; then
     wait $pid2
 else
     # Run the ros launch file if no arguments are provided
-    source install/setup.bash
-    ros2 launch nanobot_bringup robot.launch.py
+
+    # Run the FastDDS discovery server
+    fastdds discovery --server-id 0 &
+    pid1=$!
+
+    # Run the robot launch file
+    (
+        source install/setup.bash
+        ros2 launch nanobot_bringup robot.launch.py
+    ) &
+    pid2=$!
+
+    wait $pid1
+    wait $pid2
 fi
