be0ed29361d2fcb793d68fa33ff56dee8c25b19e
Alan Churichi <achurichi@gmail.com>
2025-03-05 12:25:10 -0300

    Improve scripts

--- ros_ws/scripts/control.sh
+++ ros_ws/scripts/control.sh
@@ -6,7 +6,14 @@ cd "/home/ros_ws"
 rviz2 -d ./src/nanobot_simulation/config/nanobot.rviz &
 pid1=$!
 
+# Run rqt-image-view
+# TODO: auto select the camera topic
+ros2 run rqt_image_view rqt_image_view & 
+pid2=$!
+
 # Run the teleop_twist_keyboard node
+# TODO: change the default speed
 ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args -r /cmd_vel:=/diff_controller/cmd_vel_unstamped
 
 wait $pid1
+wait $pid2
--- ros_ws/scripts/start.sh
+++ ros_ws/scripts/start.sh
@@ -3,24 +3,11 @@
 cd "/home/ros_ws"
 
 if [ "$1" == "sim" ]; then
-    # Run Gazebo and Rviz if the script is called with 'sim' as an argument
-
-    # Run Gazebo
-    (
-        source install/setup.bash
-        ros2 launch nanobot_bringup simulation.launch.py world:=./src/nanobot_simulation/worlds/boxes.world
-    ) &
-    pid1=$!
-
-    # Run rviz
-    rviz2 -d ./src/nanobot_simulation/config/nanobot.rviz &
-    pid2=$!
-
-    wait $pid1
-    wait $pid2
+    # Run Gazebo if the script is called with 'sim' as an argument
+    source install/setup.bash
+    ros2 launch nanobot_bringup simulation.launch.py world:=./src/nanobot_simulation/worlds/boxes.world
 else
     # Run the robot launch file if no arguments are provided
-
     source install/setup.bash
     ros2 launch nanobot_bringup robot.launch.py
 fi
