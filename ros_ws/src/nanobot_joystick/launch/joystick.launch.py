8c5362d9c4ea56ffded035c565026ecea13dd3af
Alan Churichi <achurichi@gmail.com>
2025-03-18 17:52:50 +0000

    Add nanobot_navigation package

--- ros_ws/src/nanobot_joystick/launch/joystick.launch.py
+++ ros_ws/src/nanobot_joystick/launch/joystick.launch.py
@@ -12,25 +12,32 @@ PACKAGE_NAME = "nanobot_joystick"
 def generate_launch_description():
     pkg_path = os.path.join(get_package_share_directory(PACKAGE_NAME))
     joystick_config_path = os.path.join(pkg_path, "config", "joystick.yaml")
+    twist_mux_config_path = os.path.join(pkg_path, "config", "twist_mux.yaml")
     
     joy_node = Node(
-        package='joy',
-        executable='joy_node',
+        package="joy",
+        executable="joy_node",
         parameters=[joystick_config_path],
     )
     
     teleop_node = Node(
-        package='teleop_twist_joy',
-        executable='teleop_node',
-        name='teleop_node',
+        package="teleop_twist_joy",
+        executable="teleop_node",
+        name="teleop_node",
         parameters=[joystick_config_path],
-        remappings=[
-            ('/cmd_vel', 'diff_controller/cmd_vel_unstamped')
-        ]
+        remappings=[("/cmd_vel", "/cmd_vel_joy")]
+    )
+    
+    twist_mux = Node(
+        package="twist_mux",
+        executable="twist_mux",
+        parameters=[twist_mux_config_path, {"use_sim_time": True}],
+        remappings=[("/cmd_vel_out", "/diff_controller/cmd_vel_unstamped")]
     )
     
     return LaunchDescription([
         joy_node,
-        teleop_node
+        teleop_node,
+        twist_mux
     ])
 
